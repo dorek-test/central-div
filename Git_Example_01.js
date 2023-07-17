0da572a72608e48ce641aab8b1d563d0b6fd78df
dorek-test <info@dorek.net>
2023-07-17 15:19:36 -0500

    First release of Git Example 01!

---
+++ Git_Example_01.js
@@ -0,0 +1,33 @@
+
+function start()
+{
+
+
+    // The Main Central Div
+    //
+    function define_and_show_main_central_div()
+    {
+        cv = dDiv(     [                "ID : Main_Div",
+                                      "Type : Size",
+                                  "Position : relative",
+                                  "Overflow : none",
+                                   "H_and_V : Center",
+                                    "Margin : 0px",
+                                   "Padding : 0px",
+                              "Border_Width : 2px",
+                                     "Width : 90vmax",
+                                    "Height : 90vmin"
+                       ]
+                 );
+    }
+    
+
+
+    let cv;
+
+    // Define and show a central div
+    //
+    define_and_show_main_central_div();
+}
+
+
