cd69daa968eaab77c35a0033b786712439fe51c3
dorek-test <info@dorek.net>
2023-07-17 15:33:24 -0500

    The 2nd with a Top Left Div using predefined 'SnackTopLeft' Animate

--- Git_Example_01.js
+++ Git_Example_01.js
@@ -3,6 +3,31 @@ function start()
 {
 
 
+    // The Inner Divs, inside the Main Div
+    //
+    // Warning: Using these effects, DO NOT USE "Position : relative",
+    //
+    function ds_02_define_and_internal_div( ID, Animate )
+    {
+        let arr = [              "ID : " + ID,
+                           "ParentID : Main_Div",
+                               "Type : Size",
+                           "Overflow : none",  // clip no hace nada por el momento
+                             "Height : none",
+                              "Width : none",
+                              "Color : Blue",
+                             "Margin : 0px",
+                            "Padding : 10px",
+                       "Border_Width : 2px",
+                            "Animate : " + Animate
+                  ];
+    
+        let cv = dDiv( arr );
+
+        cv.add_innerHTML( ID + "  -  " + Animate );
+    }
+
+
     // The Main Central Div
     //
     function define_and_show_main_central_div()
@@ -28,6 +53,11 @@ function start()
     // Define and show a central div
     //
     define_and_show_main_central_div();
+    
+
+    // The Inner Divs, inside the Main Div
+    //
+    define_and_internal_div( "Top_Left_Div",     "SnackTopLeft"     );
 }
 
 
